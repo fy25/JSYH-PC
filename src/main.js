7268bd5e6b6e67e3277dc9c827f219a6198e0a5c
fengyu <345957142@qq.com.com>
2019-04-04 01:11:34 +0800

    jsyh

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/styles/index.scss'
+import '@/icons'
+import 'normalize.css'
+import BaiduMap from 'vue-baidu-map'
+
+Vue.use(BaiduMap, {
+  /* Visit http://lbsyun.baidu.com/apiconsole/key for details about app key. */
+  ak: 'UkyCeyF5I8IxLZBaHTQKcgv4vulyoXiZ'
+})
+
+Vue.use(ElementUI);
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
